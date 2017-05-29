4279fd2062b6dc9ea1173619a6693ddc54c46b47
Keil Hunsaker <keilhun@gmail.com>
2017-05-29 12:36:55 -0400

    Finish user edit, update, index, and destroy actions

--- Gemfile
+++ Gemfile
@@ -36,6 +36,10 @@ gem 'jbuilder', '~> 2.5'
 # Use Capistrano for deployment
 # gem 'capistrano-rails', group: :development
 
+gem 'faker'
+gem 'will_paginate'
+gem 'bootstrap-will_paginate'
+
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   # Use sqlite3 as the database for Active Record
--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -2,6 +2,11 @@ require 'test_helper'
 
 class SiteLayoutTest < ActionDispatch::IntegrationTest
 
+  def setup
+    @user = users(:michael)
+    @other_user = users(:archer)
+  end
+  
   test "layout links" do
     get root_path
     assert_template 'static_pages/home'
@@ -10,5 +15,39 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
     assert_select "a[href=?]", about_path
     assert_select "a[href=?]", contact_path
     assert_select "a[href=?]", signup_path
+    
+    #check that users must be logged in to visit parts of site
+    get users_path
+    assert_redirected_to login_path
+    
+    get edit_user_path(@user)
+    assert_redirected_to login_path
+    
+    patch user_path(@other_user), params: { user: { name:  "",
+                                              email: "foo@invalid",
+                                              password:              "foo",
+                                              password_confirmation: "bar" } }
+    assert_redirected_to login_path
+    
+    #check that logged in user can only access what they are supposed to
+    log_in_as(@user)
+    get root_path
+    assert_template 'static_pages/home'
+    assert_select "a[href=?]", root_path, count: 2 #home
+    assert_select "a[href=?]", help_path          #help
+    assert_select "a[href=?]", about_path         # about
+    assert_select "a[href=?]", contact_path       # contace
+    assert_select "a[href=?]", signup_path        # signup
+    assert_select "a[href=?]", users_path         # List users
+    assert_select "a[href=?]", user_path(@user)   # show user
+    assert_select "a[href=?]", edit_user_path(@user) # Settings
+    get edit_user_path(@other_user)
+    assert_redirected_to root_url
+    patch user_path(@other_user), params: { user: { name:  "",
+                                              email: "foo@invalid",
+                                              password:              "foo",
+                                              password_confirmation: "bar" } }
+    assert_redirected_to root_url
+    
   end
 end
